a2bf890b01c052537cd0e4690fc0f9b87e1e2a93
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2024-12-20 23:01:38 +1100

    Fade in map

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
 import { sveltekit } from "@sveltejs/kit/vite";
-import { imagetools } from "@zerodevx/svelte-img/vite"
 
 /** @type {import('vite').UserConfig} */
-export default defineConfig({
-	plugins: [sveltekit(), imagetools({
-		profiles: {
-			thmb: new URLSearchParams('w=640&format=webp;jpg'),
-		}
-	})],
-})
+const config = {
+  plugins: [sveltekit()],
+};
 
+export default config;
